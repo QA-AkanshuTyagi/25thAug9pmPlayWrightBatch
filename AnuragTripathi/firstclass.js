25faeecefb630a0177b5a3b85a5885cc6c2da810
Aghori-dotcom <anurag.tripathi868@gmail.com>
2026-09-09 01:02:03 +0530

    writing first code

--- AnuragTripathi/firstclass.js
+++ AnuragTripathi/firstclass.js
@@ -1,1 +1,13 @@
 console.log("har har mahadev");
+
+// variable declaration keywords:var, let, const
+//parameters: scope
+
+
+a=12;
+b=18;
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+;
