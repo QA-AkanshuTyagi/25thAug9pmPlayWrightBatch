72488c83eb0bdf4ae94a3b01ca7afde0de51ca8e
deepak-kumar12390 <deepakchaudhary8393@gmail.com>
2026-09-12 15:57:57 +0530

    moving my code

--- dkfolder/myfirst.js
+++ dkfolder/myfirst.js
@@ -1,1 +1,46 @@
-console.log("I am a new learner in js and playright");
+/*var age =21;
+var age1 = 22;
+var age2 = 35
+console.log(age);
+console.log(age1);
+console.log(age2);
+
+
+
+let age = 20;
+let age1 = 24;
+
+console.log(age);
+console.log(age1);
+
+var a = 10;
+let b = 20;
+const c=30;
+
+function loginButtonClick(){
+var p = 100;
+let q = 200;
+const r =300;
+
+if(true){
+var x =1000; // function scope 
+let y = 2000; // function scope
+const  = 3000;// function scope
+}
+
+}*/
+
+// LOOPS 
+//sytax
+let name = "deepak"
+ for(let i=1;i<=100;i++){
+console.log("deepak")
+ }
+ let a= 10;
+ let b = 20;
+ console.log(a+b)
+ console.log(a-b)
+ console.log(a*b)
+ console.log(a/b)
+ console.log(a**b)
+ console.log(b%a)
