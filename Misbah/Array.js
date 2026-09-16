3ebe5090e1d51f6cd2875573d05506ee8154b261
Misbah9pm <misbahofficial31@gmail.com>
2026-09-16 19:55:17 +0530

    Just Checking

---
+++ Misbah/Array.js
@@ -0,0 +1,17 @@
+/*In JavaScript, an Array is a data structure used to store multiple values in a single variable.
+
+let fruits = ["Apple", "Banana", "Mango"];
+
+console.log(fruits[0]); // Apple
+console.log(fruits[1]); // Banana
+Key points
+Array index starts from 0
+Can store multiple values
+Can store different data types
+Arrays are dynamic in JavaScript
+let data = [10, "Hello", true, 25.5] ;  */
+
+
+let a = 45
+let b = 24
+console.log(a+b);
