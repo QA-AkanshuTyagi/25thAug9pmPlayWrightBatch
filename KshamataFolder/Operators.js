de3a55ad0d5aef76b08e2747a700d1442803bc09
kshamatakas-debug <kshamata.kas@gmail.com>
2026-09-17 22:39:42 +0530

    new work

---
+++ KshamataFolder/Operators.js
@@ -0,0 +1,16 @@
+let a= "kshamata"
+let b= "anurag"
+let c= "deepak"
+let d= "akanshu"
+let e= "aditi"
+
+
+let names = ["aakanshu","kshamata","aditi","anurag","deepak"]
+
+for (let eachname of names){
+    console.log(eachname);
+}
+
+
+
+
