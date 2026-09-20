abf6137311eac2583b08a70097ba15c47b7b4d9f
Sanjeev Kumar Sharma <sanjeevs604@gmail.com>
2026-09-20 17:04:55 +0530

    Updated Test of 20-09-2026

---
+++ Sanjeev_Folder/ForOfLoop_Sanjeev.js
@@ -0,0 +1,24 @@
+// For Of Loop in JavaScript
+
+//for of loop is used to iterate over the values of an iterable object like an array, string, or any other object that implements the iterable protocol. It allows you to access each value in the iterable one by one.
+
+
+
+let a = "Sanjeev";
+let b = "Prashant";
+let c = "Saurabh";
+let d = "Gaurav";
+let e = "Ankit";
+let f = "Himanshu";
+
+let names = ["Sanjeev", "Gaurav", "Ankit", "Himanshu", "Prashant", "Saurabh"];
+
+for (let eachnames of names){
+    console.log(eachnames);
+}
+
+
+
+
+console.log(a,b,d,f,c,e);
+
---
+++ Sanjeev_Folder/IntroductionOfJS.js
@@ -0,0 +1,32 @@
+//Variable declaration keywords in JavaScript are used to declare variables. 
+
+// The three main keywords are:
+
+//1. var: Used to declare a variable, can be re-declared and updated, function-scoped.
+//2. let: Used to declare a block-scoped variable, can be updated but not re-declared within the same scope.
+//3. const: Used to declare a block-scoped variable that cannot be updated or re-declared.
+
+//var
+
+age=20;
+
+a=10;
+b=15;
+c=20;
+
+console.log(a+b);
+console.log(a+c);
+console.log(b+c);
+console.log(a+b+c);
+console.log(a-b);
+console.log(b-a);
+console.log(c-b-a);
+console.log(a*b);
+console.log(a*c);
+console.log(c/a);
+console.log(b/a);
+
+
+
+
+
