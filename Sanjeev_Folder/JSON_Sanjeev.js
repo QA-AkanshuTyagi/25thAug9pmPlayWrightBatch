cdcd828c0fe67efa4aa515e4e1fef95e1deb6c81
sanjeevs604 <sanjeevs604@gmail.com>
2026-09-22 21:25:25 +0530

    Updated the code

--- Sanjeev_Folder/JSON_Sanjeev.js
+++ Sanjeev_Folder/JSON_Sanjeev.js
@@ -1,23 +1,17 @@
+
 // Object in JavaScript
 
-
-
 let boidata = {
-    fname: "Sanjeev";
-    mname: "Kumar";
-    lname: "Sharma";
-    age: 29;
-    City: "Noida";
-    Occupation: "Software Tester";
+    fname: "Sanjeev",
+    mname: "Kumar",
+    lname: "Sharma",
+    age: 29,
+    City: "Noida",
+    Occupation: "Software Tester"
 };
-
-console.log(boidata.fname);          // Output: Sanjeev
-console.log(boidata.mname);         // Output: Kumar
-console.log(boidata.lname);         // Output: Sharma
-console.log(boidata.age);           // Output: 29
-console.log(boidata.City);          // Output: Noida
-console.log(boidata.Occupation);    // Output: Software Tester  
-
-
-console.log(boidata["fname"]);      // Output: Sanjeev
-console.log()
+console.log(boidata);
+console.log(Object.keys(boidata));
+console.log(Object.values(boidata));
+for (let bio in boidata) {
+    console.log(bio, ":", boidata[bio]);
+}
